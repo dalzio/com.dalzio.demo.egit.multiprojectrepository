e0ba88312f4c61a7b3a43959888048224fd08f19
Gianpietro Dal Zio <gianpietro.dal.zio@gmail.com>
2011-11-10 11:23:51 +0100

    asdfasdf

---
+++ com.dalzio.demo.egit.multiprojectrepository.project1/src/com/dalzio/demo/egit/multiprojectrepository/project1/main/Main.java
@@ -0,0 +1,13 @@
+package com.dalzio.demo.egit.multiprojectrepository.project1.main;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ com.dalzio.demo.egit.multiprojectrepository.project2/src/com/dalzio/demo/egit/multiprojectrepository/project2/main/Main.java
@@ -0,0 +1,13 @@
+package com.dalzio.demo.egit.multiprojectrepository.project2.main;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
